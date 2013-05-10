157ae26edad2d597a2238d543487a911d78e1277
altoor <altoor@what.ever>
2013-05-10 18:26:00 +0200

    add license statement

--- examples/fetchmail.c
+++ examples/fetchmail.c
@@ -1,3 +1,20 @@
+/*
+This file is part of IPoEM.
+
+IPoEM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+IPoEM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with IPoEM. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 
 #include <curl/curl.h>
@@ -24,32 +41,25 @@ int main(int argc, char *argv[])
 	/* Set username and password */
 	snprintf(buf, 128, "%s:%s", argv[2], argv[3]);
 	curl_easy_setopt(curl, CURLOPT_USERPWD, buf);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
 
-	/* This will list every message of the given mailbox */
+	/* Get message list*/ 
 	snprintf(buf, 128, "%ss://%s@%s/%s", use_pop? "pop3":"imap", argv[2], argv[1], use_pop?"":"INBOX");
 	printf("XXXX %s\n", buf);
 	curl_easy_setopt(curl, CURLOPT_URL, buf);
-
-	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-
-	/* Perform the request, res will get the return code */ 
 	res = curl_easy_perform(curl);
-	/* Check for errors */ 
 	if(res != CURLE_OK)
-		fprintf(stderr, "curl_easy_perform() failed: %s\n",
-			  curl_easy_strerror(res));
+		fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 
+	/* Get first message content */
 	snprintf(buf, 128, "%ss://%s@%s/%s1", use_pop? "pop3":"imap", argv[2], argv[1], use_pop?"":"INBOX;UID=");
 	printf("XXXX %s\n", buf);
 	curl_easy_setopt(curl, CURLOPT_URL, buf);
 	res = curl_easy_perform(curl);
-	/* Check for errors */ 
 	if(res != CURLE_OK)
-		fprintf(stderr, "curl_easy_perform() failed: %s\n",
-			  curl_easy_strerror(res));
+		fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 
-	/* always cleanup */ 
 	curl_easy_cleanup(curl);
 	return 0;
 }
